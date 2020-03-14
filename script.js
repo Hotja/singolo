750c7d8f6fbea5f5b7eb705a7e306fa12e126eda
unknown <dudechka@mail.ru>
2020-03-14 13:44:35 +0300

    DOM:start-working

---
+++ script.js
@@ -0,0 +1,26 @@
+const NAVIGATION = document.getElementById('nav-menu');
+const BUTTON = document.getElementById('submit');
+const CLOSE_BUTTON = document.getElementById('close-submit');
+
+
+NAVIGATION.addEventListener('click', (event) => { 
+    NAVIGATION.querySelectorAll('li').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+ });
+
+ BUTTON.addEventListener('click', () => { 
+    const Subject = document.getElementById('Subject').value.toString();
+    const Describe = document.getElementById('Describe').value.toString();
+
+    document.getElementById('msg-Subject').innerText = Subject;
+    document.getElementById('msg-Describe').innerText = Describe;
+    document.getElementById('message-block').classList.remove('hidden');
+    
+ });
+
+ CLOSE_BUTTON.addEventListener('click', () => { 
+    document.getElementById('msg-Subject').innerText = '';
+    document.getElementById('msg-Describe').innerText = '';
+    document.getElementById('message-block').classList.add('hidden');
+ });
+
